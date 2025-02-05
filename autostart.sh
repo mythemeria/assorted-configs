74b3d093fb6d84beb7b4e01602facd9e76d9ecb2
mythemeria <139104088+mythemeria@users.noreply.github.com>
2025-02-05 23:27:25 +1100

    add some configs

---
+++ autostart.sh
@@ -0,0 +1,12 @@
+# set background image
+feh --bg-fill /home/izi/Pictures/bg/bg.png
+
+# start bottom panel
+lxpanel &
+nm-applet &
+
+# apparently this enables window transparency
+xcompmgr &
+
+#FUCK the stupid ass middle mouse button paste literal worst linux feature
+sxhkd &
